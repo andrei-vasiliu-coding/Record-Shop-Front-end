d9af59389287e04483882bde22c7f4ab75189807
Andrei Vasiliu <andrei-vasiliu@outlook.com>
2024-09-20 14:42:57 +0100

    feat: successfully connect to AWS cloud API

--- app/src/main/java/com/northcoders/recordshopfront_end/service/RetrofitInstance.java
+++ app/src/main/java/com/northcoders/recordshopfront_end/service/RetrofitInstance.java
@@ -8,9 +8,13 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitInstance {
 
     private static Retrofit retrofit = null;
-    private static final String BASE_URL = "http://172.28.86.55:8080/api/v1/recordShop/";
+    //private static final String BASE_URL = "http://172.28.86.55:8080/api/v1/recordShop/";
     //private static final String BASE_URL = "http://localhost:8080/api/v1/recordShop/";
-    //private static final String BASE_URL = "http://RecordShopAPI-env.eba-zw3vmncw.eu-west-2.elasticbeanstalk.com/api/v1/recordShop/";
+    private static final String BASE_URL = "http://RecordShopAPI-env.eba-zw3vmncw.eu-west-2.elasticbeanstalk.com/api/v1/recordShop/";
+    //also update network_security_config with either:
+    //172.28.86.55
+    //or
+    //recordshopapi-env.eba-zw3vmncw.eu-west-2.elasticbeanstalk.com
 
     public static AlbumApiService getService() {
 
